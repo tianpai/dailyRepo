cd369a171594ef7447b5e30228e748fed4024333
tianpai <tianpai.z@icloud.com>
2025-04-22 20:17:46 -0400

    backend: add utils functions

---
+++ backend/utils/time.js
@@ -0,0 +1,17 @@
+export function getTodayUTC() {
+  const now = new Date();
+  const yyyy = now.getUTCFullYear();
+  const mm = String(now.getUTCMonth() + 1).padStart(2, "0");
+  const dd = String(now.getUTCDate()).padStart(2, "0");
+
+  return `${yyyy}-${mm}-${dd}`;
+}
+
+export function getDateFromTimestamp(dateStr) {
+  const date = new Date(dateStr);
+  const yyyy = date.getUTCFullYear();
+  const mm = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const dd = String(date.getUTCDate()).padStart(2, "0");
+
+  return `${yyyy}-${mm}-${dd}`;
+}
