60e3b7edf346f257cac2b87e5ad7d20e9be41149
Tianpai <tianpai.zhang@alumni.utoronto.ca>
2025-05-29 21:19:44 -0400

    redesign: add /history, /latest, /ranking

--- backend/routes/RepoRoutes.js
+++ backend/routes/RepoRoutes.js
@@ -1,8 +1,37 @@
 import { Router } from "express";
-import * as RepoController from "../controller/RepoController.js";
+import {
+  getLatest,
+  getHistory,
+  getRanking,
+} from "../controller/RepoController.js";
 
 const repoRouter = Router();
 
-repoRouter.get("/trending", RepoController.getRepoTopTrending);
+async function notImplemented(_, res) {
+  res.status(501).json({ error: "Not implemented" });
+}
+
+/**
+ * GET /api/repo/latest
+ * Purpose: Get the latest trending repos
+ * No query parameters
+ *
+ * It should return today's if available, otherwise the most recent available
+ */
+repoRouter.get("/latest", getLatest);
+
+/**
+ * GET /api/repo/historical?date=YYYY-MM-DD
+ * Purpose: Get trending repos for a specific historical date
+ * Query parameter: date (required, in YYYY-MM-DD format)
+ */
+repoRouter.get("/history", getHistory);
+
+/**
+ * GET /api/repo/ranking
+ * Purpose: Get the top 10 repos of all time (based on some aggregation)
+ * Optional query parameter: top (number, to get top N repos)
+ */
+repoRouter.get("/ranking", getRanking);
 
 export default repoRouter;
