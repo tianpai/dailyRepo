058e87d4ec24fe716618f4c95af6f0797bca1f4b
Tianpai <tianpai.zhang@alumni.utoronto.ca>
2025-05-30 19:50:17 -0400

    feat: valiadte date and get date

--- backend/utils/time.js
+++ backend/utils/time.js
@@ -7,6 +7,17 @@ export function getTodayUTC() {
   return `${yyyy}-${mm}-${dd}`;
 }
 
+export function getYesterdayUTC() {
+  const now = new Date();
+  now.setUTCDate(now.getUTCDate() - 1);
+
+  const yyyy = now.getUTCFullYear();
+  const mm = String(now.getUTCMonth() + 1).padStart(2, "0");
+  const dd = String(now.getUTCDate()).padStart(2, "0");
+
+  return `${yyyy}-${mm}-${dd}`;
+}
+
 export function getDateFromTimestamp(dateStr) {
   const date = new Date(dateStr);
   const yyyy = date.getUTCFullYear();
@@ -14,4 +25,59 @@ export function getDateFromTimestamp(dateStr) {
   const dd = String(date.getUTCDate()).padStart(2, "0");
 
   return `${yyyy}-${mm}-${dd}`;
+}
+
+// utils/validateDate.js
+export function isValidDate(str) {
+  const m = /^(\d{4})-(\d{2})-(\d{2})$/.exec(str);
+  if (!m) return false;
+
+  const y = +m[1],
+    mth = +m[2],
+    d = +m[3];
+  if (y < 2024 || mth < 1 || mth > 12) return false;
+  const days = new Date(y, mth, 0).getDate();
+  return d >= 1 && d <= days;
+}
+
+/**
+ * matching YYYY-MM-DD format
+ * also consider leap years and valid days in month
+ * YYYY must be greater than 2024
+ */
+export function validateDate(date) {
+  const m = /^(\d{4})-(\d{2})-(\d{2})$/.exec(date);
+  if (!m) return null;
+
+  let [_, y, M, d] = m;
+  const year = +y;
+  const month = +M;
+  const day = +d;
+
+  /*
+   * earliest date is 2025-01-01
+   */
+  if (year <= 2024) return null;
+  if (month < 1 || month > 12) return null;
+
+  // days in each month
+  const isLeap = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  const daysInMonth = [
+    31,
+    isLeap ? 29 : 28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  if (day < 1 || day > daysInMonth[month - 1]) return null;
+
+  return date;
 }
