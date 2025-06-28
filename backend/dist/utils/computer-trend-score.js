0e967b1c952f1ed9b69c9e6557221357403ff39f
Tianpai <tianpai.zhang@alumni.utoronto.ca>
2025-06-28 19:40:16 -0400

    Remove dist directory from version control

--- backend/dist/utils/computer-trend-score.js
+++
@@ -1,5 +0,0 @@
-export function calculateTrendScore({ stars, previousTrend = 0 }) {
-    //TODO: temporary solution, replace with real trend score calculation
-    return stars;
-}
-//# sourceMappingURL=computer-trend-score.js.map
